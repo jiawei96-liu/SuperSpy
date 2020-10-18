4bbb1c433a1be55e74a82e17191d788e4c9bfb45
jiawei.liu <jiawei96.liu@outlook.com>
2020-10-18 19:37:02 +0800

    init version

---
+++ app/src/main/java/com/software/ustc/superspy/WelcomeActivity.java
@@ -0,0 +1,35 @@
+package com.software.ustc.superspy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.software.ustc.superspy.ui.login.LoginActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private final int WELCONE_DISPLAY_LENGHT = 3000; // 3秒后进入系统
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
+        getSupportActionBar().hide();//隐藏标题栏
+        setContentView(R.layout.activity_welcome);
+        Thread myThread=new Thread(){//创建子线程
+            @Override
+            public void run() {
+                try{
+                    sleep(WELCONE_DISPLAY_LENGHT);//使程序休眠五秒
+                    Intent it=new Intent(getApplicationContext(), LoginActivity.class);//启动MainActivity
+                    startActivity(it);
+                    finish();//关闭当前活动
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();//启动线程
+    }
+}
