4d40c43d3155deeb7e18d19ab62e4705d437ceeb
mengrui.liu <1318681240@qq.com>
2020-10-19 00:19:41 +0800

    一些简单修改

--- app/src/main/java/com/software/ustc/superspy/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/software/ustc/superspy/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,7 @@ import androidx.lifecycle.ViewModelProviders;
 import com.software.ustc.superspy.R;
 
 public class NotificationsFragment extends Fragment {
+
 
     private NotificationsViewModel notificationsViewModel;
 
