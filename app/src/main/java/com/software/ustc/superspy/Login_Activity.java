f19d7ddea2140f9df0471c79fe24e540fb8bbadd
mengrui.liu <1318681240@qq.com>
2020-10-26 21:43:46 +0800

    纠正逻辑错误

--- app/src/main/java/com/software/ustc/superspy/Login_Activity.java
+++ app/src/main/java/com/software/ustc/superspy/Login_Activity.java
@@ -19,32 +19,29 @@ import java.util.List;
 
 public class Login_Activity extends BaseActivity {
 
-    private Button button1;
+    private Button login;
     private CheckBox rememberpass;
     private EditText accountEdit;
     private  EditText passwordEdit;
-    private Button button2;
+    private Button roll;
 
     int flag=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login__activity);
-
-
         //LitePal.initialize(this);
-
-        button1=findViewById(R.id.btn_login);
-        button2=findViewById(R.id.et_0);
-        accountEdit=findViewById(R.id.et_1);
-        passwordEdit=findViewById(R.id.et_2);
+        login=findViewById(R.id.bLogin);
+        roll=findViewById(R.id.cRoll);
+        accountEdit=findViewById(R.id.eUsername);
+        passwordEdit=findViewById(R.id.ePassward);
         rememberpass=findViewById(R.id.cb_remember_pwd);
 
-        Login_data login_id1 = LitePal.find(Login_data.class, 1);
-        accountEdit.setText(login_id1.getUsername());
-        passwordEdit.setText(login_id1.getPassward());
+        Login_data loginId1 = LitePal.find(Login_data.class, 1);
+        accountEdit.setText(loginId1.getUsername());
+        passwordEdit.setText(loginId1.getPassward());
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -60,6 +57,8 @@ public class Login_Activity extends BaseActivity {
                         break;
                     }
                 }
+                if(account.equals("") && password.equals(""))
+                    flag=0;
                 if (flag == 1) {
                     if (rememberpass.isChecked()) {
                         Login_data login1 = new Login_data();
@@ -67,10 +66,17 @@ public class Login_Activity extends BaseActivity {
                         login1.setPassward(password);
                         login1.updateAll("id=?", "1");
                     }
+                    else{
+                        Login_data login1 = new Login_data();
+                        login1.setUsername("");
+                        login1.setPassward("");
+                        login1.updateAll("id=?", "1");
+                    }
                     Toast.makeText(Login_Activity.this, "登录成功", Toast.LENGTH_SHORT).show();
 //                    Intent intent1 = new Intent(Login_Activity.this, MainActivity.class);
                     Intent intent1 = new Intent(Login_Activity.this, AppInfoShow.class);
                     startActivity(intent1);
+                    finish();
                 } else {
                     Toast.makeText(Login_Activity.this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
                 }
@@ -82,11 +88,12 @@ public class Login_Activity extends BaseActivity {
 
 
         });
-        button2.setOnClickListener(new View.OnClickListener() {
+        roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Login_Activity.this,roll_Activity.class);
                 startActivity(intent);
+
             }
         });
     }
