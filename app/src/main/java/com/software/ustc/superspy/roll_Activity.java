b7b7e75472481df9c3d6ef09de92d6245a806c60
mengrui.liu <1318681240@qq.com>
2020-10-27 23:06:49 +0800

    逻辑修正

--- app/src/main/java/com/software/ustc/superspy/roll_Activity.java
+++ app/src/main/java/com/software/ustc/superspy/roll_Activity.java
@@ -10,6 +10,10 @@ import android.widget.Toast;
 import com.software.ustc.superspy.kits.BaseActivity;
 import com.software.ustc.superspy.kits.Login_data;
 
+import org.litepal.LitePal;
+
+import java.util.List;
+
 public class roll_Activity extends BaseActivity {
 
     private Button roll;
@@ -28,15 +32,26 @@ public class roll_Activity extends BaseActivity {
         roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int flag=0;
                 Toast toast = Toast.makeText(roll_Activity.this, null, Toast.LENGTH_SHORT);
                 String passward = passwordRollEdit.getText().toString();
                 String passwardConfirm = passwordConfirm.getText().toString();
                 String account1 = accountRollEdit.getText().toString();
+                List<Login_data> logins = LitePal.findAll(Login_data.class);
+                for (Login_data data1 : logins) {
+                    if (account1.equals(data1.getUsername()) ) {
+                        flag = 1;
+                        break;
+                    }
+                }
                 if (!(passward.equals(passwardConfirm))) {
                     toast.setText("两次输入的密码不一致");
                 } else if (passward.equals("") || account1.equals("")) {
                     toast.setText("用户名或密码为空");
-                } else {
+                } else if(flag==1){
+                    toast.setText("用户名已被注册");
+                }
+                else {
                     toast.setText("注册成功");
                     Login_data login = new Login_data();
                     login.setUsername(account1);
